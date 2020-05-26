f8c581b6e1cf80b0daafe5ef4f88509fe38e44dd
Jose  Coto Zevallos <jose@Joses-MacBook-Air.local>
2020-05-26 21:46:55 +0200

    Anadir fuente de data y data limpia subida el 2020-05-26

--- limpiar_data/limpiar_data_covid.py
+++ limpiar_data/limpiar_data_covid.py
@@ -33,6 +33,137 @@ ubigeo_df = pd.read_csv(
     "../data/original/ubigeo_distritos.csv", dtype={"ubigeo": "string"}
 )
 
+
+def col_a_minusculas(df):
+    df.columns = [col.lower() for col in df.columns]
+
+
+def uniformizar_data(df):
+    """Quitar mayusculas y caracteres especiales como 
+    tildes"""
+    df[["departamento", "provincia", "distrito"]] = df[
+        ["departamento", "provincia", "distrito"]
+    ].applymap(lambda x: unidecode.unidecode(x) if isinstance(x, str) else x)
+    df[["sexo", "departamento", "provincia", "distrito"]] = df[
+        ["sexo", "departamento", "provincia", "distrito"]
+    ].apply(lambda x: x.str.title().str.strip())
+    return df
+
+
+def helper_add_ubigeo(df, ubigeo_df):
+    # Ayuda para unir localiazaciones con ubigeo
+    # retorna los casos donde ubigeo no se encuentra
+    # para uniformizar la data
+    localizacion = (
+        df.groupby(["departamento", "provincia", "distrito"], as_index=False)
+        .count()
+        .loc[:, ["departamento", "provincia", "distrito"]]
+    )
+    merge_ubigeos = localizacion.merge(
+        ubigeo_df, how="left", on=["departamento", "provincia", "distrito"]
+    )
+    return merge_ubigeos.loc[merge_ubigeos.ubigeo.isnull()]
+
+
+# +
+Dcorreccion = namedtuple(
+    "RemplazarDistrito", ["departamento", "provincia", "distrito", "cambio"]
+)
+Pcorreccion = namedtuple(
+    "RemplazarDepartamento", ["departamento", "provincia", "cambio"]
+)
+# Casos para correcciones de diferencias entre distrito en la
+# data y distrito en nuestra base de ubigeos
+correcciones_dist = [
+    ("Apurimac", "Chincheros", "Anco_Huallo", "Anco-Huallo"),
+    (
+        "Ayacucho",
+        "Huamanga",
+        "Andres Avelino Caceres",
+        "Andres Avelino Caceres Dorregaray",
+    ),
+    ("Huanuco", "Huanuco", "Quisqui (Kichki)", "Quisqui"),
+    ("Piura", "Sechura", "Rinconada Llicuar", "Rinconada-Llicuar"),
+    ("San Martin", "Picota", "Caspisapa", "Caspizapa"),
+    (
+        "Tacna",
+        "Tacna",
+        "Coronel Gregorio Albarracin L.",
+        "Coronel Gregorio Albarracin Lanchipa",
+    ),
+    ("Ucayali", "Atalaya", "Raymondi", "Raimondi"),
+    (
+        "Callao",
+        "Prov. Const. Del Callao",
+        "Carmen De La Legua Reynoso",
+        "Carmen De La Legua-Reynoso",
+    ),
+    (
+        "Ayacucho",
+        "Huamanga",
+        "Andres Avelino Caceres",
+        "Andres Avelino Caceres Dorregaray",
+    ),
+    (
+        "Ayacucho",
+        "Huamanga",
+        "Andres Avelino Caceres D.",
+        "Andres Avelino Caceres Dorregaray",
+    ),
+    (
+        "Tacna",
+        "Tacna",
+        "Coronel Gregorio Albarracin L.",
+        "Coronel Gregorio Albarracin Lanchipa",
+    ),
+    (
+        "Puno",
+        "Sandia",
+        "San Pedro De Putina Puncu",
+        "San Pedro De Putina Punco",
+    ),
+    ("Ica", "Nazca", "Nazca", "Nasca"),
+]
+
+correcciones_prov = [
+    ("Ica", "Nazca", "Nasca"),
+    ("Callao", "Prov. Const. Del Callao", "Callao"),
+    ("Ica", "Nazca", "Nasca"),
+]
+
+
+def corregir_distritos(df, correcciones):
+    """Ayuda para remover diferencias en distritos y permitir
+    mejor asociacion entre casos y ubigeo"""
+    for correccion in correcciones:
+        data_correccion = Dcorreccion(*correccion)
+        df.loc[
+            (df["departamento"] == data_correccion.departamento)
+            & (df["provincia"] == data_correccion.provincia)
+            & (df["distrito"] == data_correccion.distrito),
+            "distrito",
+        ] = data_correccion.cambio
+
+
+def corregir_provincias(df, correcciones):
+    """Ayuda para remover diferencias en provincias y permitir
+    mejor asociacion entre casos y ubigeo"""
+    for correccion in correcciones:
+        data_correccion = Pcorreccion(*correccion)
+        df.loc[
+            (df["departamento"] == data_correccion.departamento)
+            & (df["provincia"] == data_correccion.provincia),
+            "provincia",
+        ] = data_correccion.cambio
+
+
+# -
+
+# En algunos casos parece que los meses y dias se han introducido al revés
+def cambiar_a_mes_dia(columna):
+    return pd.to_datetime(columna.dt.strftime("%Y-%d-%m"), format="%Y-%m-%d")
+
+
 # Cambiando mayusculas
 casos_df.columns = [col.lower() for col in casos_df.columns]
 casos_df[["sexo", "departamento", "provincia", "distrito"]] = casos_df[
@@ -203,11 +334,9 @@ data_05_24.columns = [col.lower() for col in data_05_24.columns]
 data_05_24[["departamento", "provincia", "distrito"]] = data_05_24[
     ["departamento", "provincia", "distrito"]
 ].applymap(lambda x: unidecode.unidecode(x) if isinstance(x, str) else x)
-data_05_24[
+data_05_24[["sexo", "departamento", "provincia", "distrito"]] = data_05_24[
     ["sexo", "departamento", "provincia", "distrito"]
-] = data_05_24[["sexo", "departamento", "provincia", "distrito"]].apply(
-    lambda x: x.str.title().str.strip()
-)
+].apply(lambda x: x.str.title().str.strip())
 
 # +
 # Correcciones en distritos y provincias para uniformizar nombres
@@ -295,9 +424,7 @@ posteriores_fecha_data = data_05_24.fecha_resultado > "2020-05-24"
 data_05_24.loc[antes_primer_caso, "fecha_resultado"] = cambiar_a_mes_dia(
     data_05_24.loc[antes_primer_caso, "fecha_resultado"]
 )
-data_05_24.loc[
-    posteriores_fecha_data, "fecha_resultado"
-] = cambiar_a_mes_dia(
+data_05_24.loc[posteriores_fecha_data, "fecha_resultado"] = cambiar_a_mes_dia(
     data_05_24.loc[posteriores_fecha_data, "fecha_resultado"]
 )
 data_05_24 = data_05_24[
@@ -308,4 +435,39 @@ data_05_24 = data_05_24[
 
 data_05_24.to_csv(
     "../data/limpia/data_limpia_datos_siscovid_2020_05_24.csv", index=False
+)
+
+# ### Nueva data 2020-05-26
+
+data_05_26 = pd.read_csv(
+    "../data/original/datos_abiertos_siscovid_2020_05_26.csv",
+    parse_dates=["FECHA_RESULTADO"],
+    encoding="latin",
+    dtype={"EDAD": pd.Int64Dtype()},
+)
+
+col_a_minusculas(data_05_26)
+
+data_05_26 = uniformizar_data(data_05_26)
+
+corregir_distritos(data_05_26, correcciones_dist)
+
+corregir_provincias(data_05_26, correcciones_prov)
+
+antes_primer_caso = data_05_26.fecha_resultado < "2020-03-06"
+posteriores_fecha_data = data_05_26.fecha_resultado > "2020-05-26"
+data_05_26.loc[antes_primer_caso, "fecha_resultado"] = cambiar_a_mes_dia(
+    data_05_26.loc[antes_primer_caso, "fecha_resultado"]
+)
+data_05_26.loc[posteriores_fecha_data, "fecha_resultado"] = cambiar_a_mes_dia(
+    data_05_26.loc[posteriores_fecha_data, "fecha_resultado"]
+)
+data_05_26 = data_05_26[
+    (data_05_26.fecha_resultado >= "2020-03-06")
+    | (data_05_26.fecha_resultado <= "2020-05-24")
+    | (data_05_26.fecha_resultado.isnull())
+]
+
+data_05_26.to_csv(
+    "../data/limpia/data_limpia_datos_siscovid_2020_05_26.csv", index=False
 )
